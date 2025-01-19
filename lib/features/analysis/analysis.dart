5e01b4f6d5665d292e84fb83e3d4a1ea0395cec2
SimonLiu <simonliu423@gmail.com>
2025-01-19 15:48:33 +0800

    Feat: implement jaundice analysis page

--- lib/features/analysis/analysis.dart
+++ lib/features/analysis/analysis.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class AnalysisPage extends StatelessWidget {
@@ -5,8 +7,92 @@ class AnalysisPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Analysis')),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/n_background2.png'),
+          fit: BoxFit.cover,
+          opacity: 1,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text('Analysis'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  final buttonWidth =
+                      (constraints.maxWidth - 30) / 5; // Account for padding
+                  final iconSize =
+                      buttonWidth * 0.9; // Slightly smaller than container
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        iconSize: buttonWidth,
+                        icon: Image.asset(
+                          'assets/images/n_home.png',
+                          height: iconSize,
+                        ),
+                        onPressed: () {
+                          log('home pressed');
+                        },
+                      ),
+                      IconButton(
+                        iconSize: buttonWidth,
+                        icon: Image.asset(
+                          'assets/images/n_rotate.png',
+                          height: iconSize,
+                        ),
+                        onPressed: () {
+                          log('rotate pressed');
+                        },
+                      ),
+                      IconButton(
+                        iconSize: buttonWidth,
+                        icon: Image.asset(
+                          'assets/images/n_analysis.png',
+                          height: iconSize,
+                        ),
+                        onPressed: () {
+                          log('analysis pressed');
+                        },
+                      ),
+                      IconButton(
+                        iconSize: buttonWidth,
+                        icon: Image.asset(
+                          'assets/images/n_result.png',
+                          height: iconSize,
+                        ),
+                        onPressed: () {
+                          log('result pressed');
+                        },
+                      ),
+                      IconButton(
+                        iconSize: buttonWidth,
+                        icon: Image.asset(
+                          'assets/images/n_reset1.png',
+                          height: iconSize,
+                        ),
+                        onPressed: () {
+                          log('reset pressed');
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
