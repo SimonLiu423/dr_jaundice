fb43cbaad152d5a4cfb521db2e0e35c0bfcad489
SimonLiu <simonliu423@gmail.com>
2025-04-12 16:19:18 +0800

    Fix: Start analysis image request param

--- lib/features/analysis/bloc/analysis_bloc.dart
+++ lib/features/analysis/bloc/analysis_bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import '../../../core/constants.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,12 +16,14 @@ class AnalysisBloc extends Bloc<AnalysisEvent, AnalysisState> {
   final dio = Dio();
   File? image;
 
-  Future<void> _onLoadImage(LoadImage event, Emitter<AnalysisState> emit) async {
+  Future<void> _onLoadImage(
+      LoadImage event, Emitter<AnalysisState> emit) async {
     image = event.image;
     emit(ImageLoaded(image: image!));
   }
 
-  Future<void> _onStartAnalysis(StartAnalysis event, Emitter<AnalysisState> emit) async {
+  Future<void> _onStartAnalysis(
+      StartAnalysis event, Emitter<AnalysisState> emit) async {
     emit(AnalysisLoading());
 
     // TODO(SimonLiu423): Revise this part when the API is ready
@@ -31,7 +32,7 @@ class AnalysisBloc extends Bloc<AnalysisEvent, AnalysisState> {
     // final result = await dio.post(
     //   '$apiUrl/analyze',
     //   data: FormData.fromMap({
-    //     'image': MultipartFile.fromFileSync(event.image.path),
+    //     'image': MultipartFile.fromFileSync(image!.path),
     //   }),
     // );
 
